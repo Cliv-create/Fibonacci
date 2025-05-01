84d700ed96298b8dfa5c453f054f97cb31dc20d8
Cliv-create <66003937+Cliv-create@users.noreply.github.com>
2025-05-01 15:50:39 +0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Fibonacci
+{
+    using System.Runtime.InteropServices;
+    using static System.Runtime.InteropServices.JavaScript.JSType;
+
+    namespace Fibonacci
+    {
+        internal class Program
+        {
+            static void FibonacciCalculation(ulong upper_limit)
+            {
+                if (upper_limit == 0) Console.WriteLine("Error! Upper limit is 0.");
+
+                ulong fib = 0, temp1 = 1, temp2 = 0;
+
+                while (true)
+                {
+                    fib = checked(temp1 + temp2);
+                    if (fib > upper_limit) break;
+                    Console.WriteLine(fib);
+                    temp1 = temp2;
+                    temp2 = fib;
+                }
+
+                Console.WriteLine("Done! Calculated number: " + temp2);
+            }
+            static void Main(string[] args)
+            {
+                while (true)
+                {
+                    string? user_input = "";
+                    ulong input_converted = 0;
+
+                    Console.WriteLine("Fibonacci number calculation started!");
+                    Console.WriteLine("Enter upper fibonacci number limit (type exit to stop): ");
+                    user_input = Console.ReadLine();
+                    if (!(user_input != null))
+                    {
+                        Console.WriteLine("Input was null! Defaulting to 100...");
+                        input_converted = 100;
+                    }
+                    if (user_input.ToLower() == "exit")
+                    {
+                        Console.WriteLine("Exiting application!");
+                        break;
+                    }
+                    if (!ulong.TryParse(user_input, out input_converted))
+                    {
+                        Console.WriteLine("Failed to parse the number! Defaulting to 100...");
+                        input_converted = 100;
+                    }
+                    try
+                    {
+                        Thread t1 = new Thread(() => FibonacciCalculation(input_converted));
+                        t1.Start();
+                    }
+                    catch (OverflowException oex)
+                    {
+                        Console.WriteLine("Number overflow happened! Did you input a number bigger then 18 446 744 073 709 551 615?");
+                        Console.WriteLine($"Exception message: {oex.Message}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Unexpected error happened! {ex.Message}\nStack trace: {ex.StackTrace}");
+                    }
+                }
+            }
+        }
+    }
+
+}
